183faa9e40e3261b059b46d4006ebda510865f1a
Ashish Juneja <43455081+junejaapi@users.noreply.github.com>
2018-10-02 02:31:15 -0400

    Update config.js
    Hiding my access tokens I hope nobody used them.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports= {
-    consumer_key:         'FPsFkhvFuiF6aDo577GG98E1W',
-    consumer_secret:      'dIBsVdHpEcLoI0qrXDMyJDWba5JEVhk9xmFrotN52UbgBg7WJ3',
-    access_token:         '1011251978272305154-lAfF0McNu7qySNW8gu2PYJTO4yO3dr',
-    access_token_secret:  'Kc4oMlmvVbsvCt8IiQDFjTuMKhQR2XPNRUvGDFV9gXrEa',
+    consumer_key:         '...',
+    consumer_secret:      '...',
+    access_token:         '...',
+    access_token_secret:  '...',
 }
